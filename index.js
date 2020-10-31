a3bb6d98dd7b93122d9d364b26cbf5c9e4c20d65
Hillary Gil <hillarygil97@gmail.com>
2020-10-31 17:39:30 -0600

    Diferencias y declaración de variables, tambien un poco de sintaxis

---
+++ index.js
@@ -0,0 +1,24 @@
+/*
+ Existen 3 tipos de varaiables en javascript
+*/
+
+const PI = 3.1416  //const declara constantes, es decir variables que jamás cambiaran de valor en todo el programa.
+var b = 'Hillary' //var declara variables globales que podran ser usadas en cualquier parte del codigo.
+let a = 'Anahi' //let declara varaibles locales que solo son usadas en sus modulos.
+
+//javaScript se encarga de auto detectar el tipo de dato al que pertenece cada variable por lo que no es necesario
+//hacer uso de int, float, string o array al principio de la declaración de variables.
+
+var name = 'Dana'
+let name2 = 'Edwin'
+const NAME3 = 'Hillary' // por buenas practicas los nombres de constantes se ponen en mayusculas.
+
+function sayHello(){
+    let name = 'Hill'
+    let name2 = 'Naomi'
+    console.log(name,name2)
+}
+
+sayHello()
+console.log(`Nombre 1: ${name}`)//esta forma de mandar a imprimir en consola se le conoce como 
+console.log(`Nombre 2: ${name2}`) //        TEMPLATE LITERAL
